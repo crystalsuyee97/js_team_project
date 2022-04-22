0849a1fca234f904fe2fc1e5aa1ad6bd56855dbd
Crystal <cyap@deloitte.co.uk>
2022-04-22 15:19:53 +0100

    rotation setting for caesar cipher

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 window.onload = function() {
   var currentDate = new Date();
-  var quote = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+  var quote = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   document.getElementById("date").innerHTML = currentDate.toLocaleDateString() + " " + currentDate.toLocaleTimeString();
   document.getElementById("quote").innerHTML = quote;
+
+  var rotationSetting = document.getElementById("caesarSetting");
+  rotationSetting.addEventListener('input', function handleChange(event) {
+    rotationSetting.value = event.target.value;
+    document.getElementById("caesarText").innerHTML = caesarCipher(newInput, Number(rotationSetting.value));
+  })
+  var newInput = document.getElementById("textInput");
+  newInput.addEventListener('input', function handleChange(event) {
+    newInput = event.target.value;
+    document.getElementById("caesarText").innerHTML = caesarCipher(newInput, Number(rotationSetting.value));
+  });
 }
 
 // const alpha = Array.from(Array(26)).map((e, i) => i + 65);
@@ -10,11 +21,6 @@ window.onload = function() {
 // const stringArray = (inputText.toLowerCase()).split("")
 // console.log(stringArray)
 // console.log(inputText.charCodeAt())
-var newInput = document.getElementById("textInput");
-newInput.addEventListener('input', function handleChange(event) {
-  newInput = event.target.value;
-  caesarCipher(newInput, 2);
-});
 
 function caesarCipher(value, n) {
   let tempArray = [];
@@ -28,8 +34,7 @@ function caesarCipher(value, n) {
       tempArray.push(String.fromCharCode(characterCode));
     }
   }
-  var newString = tempArray.join("")
-  console.log(newString)
+  return tempArray.join("");
 }
 
 class Header extends HTMLElement {
@@ -47,22 +52,38 @@ class Footer extends HTMLElement {
     this.innerHTML = `
     <footer>
       <span id="date"></span>
-      <span id ="quote">Hello</span>
+      <span id ="quote"></span>
     </footer>  
     `;
   }
 }
 
-class Column extends HTMLElement {
+class Columns extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
+    <span class="cipherColumn">
+      <p class="columnTitle">Caesar Cipher</p>
+      <div class="columnTitle">Rotation setting: 
+        <input type="number" min=0 id="caesarSetting" value="0"></input>
+      </div>
+      <textarea type="text" id="caesarText"></textarea> 
+    </span class="cipherColumn">
     <div class="cipherColumn">
-      <textarea type="text"></textarea>
-    </div>   
+      <span>
+        <p class="columnTitle">ROT13 Cipher</p>
+        <textarea type="text"></textarea> 
+      </span> 
+    </div> 
+    <div class="cipherColumn">
+      <span>
+        <p class="columnTitle">Vigenere Cipher</p>
+        <textarea type="text"></textarea> 
+      </span> 
+    </div>
     `;
   }
 }
 
 customElements.define('main-header', Header);
 customElements.define('main-footer', Footer);
-customElements.define('cipher-column', Column);
+customElements.define('cipher-columns', Columns);
