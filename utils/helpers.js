a7cac73fa282146b54bbcff700311f3b5074d586
Crystal <cyap@deloitte.co.uk>
2022-05-02 10:37:16 +0100

    removed moment package

---
+++ utils/helpers.js
@@ -0,0 +1,3 @@
+export const checkUppercase = (characterCode) => characterCode > 64 && characterCode < 91;
+
+export const checkLowercase = (characterCode) => characterCode > 96 && characterCode < 123;
